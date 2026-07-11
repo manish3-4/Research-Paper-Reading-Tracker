c90d47d724c85fb73ae62aeaae341fb68ddda6ee
manish3-4 <preetisikka553@gmail.com>
2026-07-11 14:31:49 +0530

    major bug fixes in deployment.

--- server.js
+++ server.js
@@ -26,19 +26,6 @@ app.use(express.static(frontendDistPath));
 //connection database
 await connectDatabase();
 
-// app.get('*', async (req, res) => {
-//   try {
-//     console.log(frontendDistPath);
-//     const html = await readFile(join(frontendDistPath, 'index.html'), 'utf-8');
-//     res.type('html').send(html);
-//   } catch (error) {
-//     res.status(404).send('Error in frontend path');
-//   }
-// });
-
-app.get("*", async (req, res) => {
-  res.sendFile(join(frontendDistPath, "index.html"));
-});
 
 app.get('/api/papers', async (req, res) => {
   try {
@@ -178,4 +165,11 @@ app.get('/api/health', (req, res) => {
 });
 
 // Serve the SPA entry point for all non-API routes
+app.get('*', async (req, res) => {
+  try {
+    res.sendFile(join(frontendDistPath, 'index.html'));
+  } catch (error) {
+    res.status(404).send('Error in frontend path');
+  }
+});
 
